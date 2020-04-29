80a894aaa9dd1b952a8f0790d54bcfe1642757d0
PeteTimesSix <Pete1001@seznam.cz>
2020-04-29 14:52:57 +0200

    init commit

---
+++ Source/KeepColorsSeparate/KeepColorsSeparate/ColoredStackingPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace KeepColorsSeparate
+{
+    [HarmonyPatch(typeof(Thing))]
+    static class ColoredStackingPatch
+    {
+        [HarmonyPatch(nameof(Thing.CanStackWith)), HarmonyPostfix]
+        static void Thing_CanStackWith_Postfix(ref bool __result, Thing other, Thing __instance)
+        {
+            if (__result)
+            {
+                var colorableThis = __instance.TryGetComp<CompColorable>();
+                var colorableThat = other.TryGetComp<CompColorable>();
+                if (colorableThis != null && colorableThat != null)
+                {
+                    Log.Message("checking...");
+                    __result = colorableThis.Color.IndistinguishableFrom(colorableThat.Color);
+                }
+            }
+            return;
+        }
+    }
+}
---
+++ Source/KeepColorsSeparate/KeepColorsSeparate/KeepColorsSeparateMod.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace KeepColorsSeparate
+{
+    public class KeepColorsSeparateMod: Mod 
+    {
+        public KeepColorsSeparateMod(ModContentPack content): base(content)
+        {
+            Log.Message("loaded");
+            var harmony = new Harmony("PeteTimesSix.KeepColorsSeparate");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
